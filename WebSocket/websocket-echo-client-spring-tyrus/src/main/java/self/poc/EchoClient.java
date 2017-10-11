dec72a43c9ba93da8a7b8753221c5379267bd9ac
Arvind Srivastava 3 <asri68@sapient.com>
2017-10-11 14:07:53 +0530

    Implemented Retryable WebSocket connection manager

--- WebSocket/websocket-echo-client-spring-tyrus/src/main/java/self/poc/EchoClient.java
+++ WebSocket/websocket-echo-client-spring-tyrus/src/main/java/self/poc/EchoClient.java
@@ -4,13 +4,16 @@ import java.util.concurrent.CountDownLatch;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.web.socket.client.WebSocketConnectionManager;
+
+import self.poc.client.RetryableWebSocketConnectionManager;
 
 public class EchoClient {
 
 	public static void main(String[] args) throws InterruptedException {
 		ApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-		WebSocketConnectionManager connectionManager = context.getBean("webSocketConnectionManager", WebSocketConnectionManager.class);
+		RetryableWebSocketConnectionManager connectionManager = context.getBean("webSocketConnectionManager", RetryableWebSocketConnectionManager.class);
+		connectionManager.startWithRetry();
+		//connectionManager.start();
 		System.out.println("Connection Manager status : " + connectionManager.isRunning());		
 		
 		new CountDownLatch(1).await();
